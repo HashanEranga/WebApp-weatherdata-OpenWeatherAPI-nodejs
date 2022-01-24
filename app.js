971b1d4739508d94b562c5a2964290cc5d519917
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-01-24 17:34:44 +0530

    feat(): Open API call

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const app = exp()
 const PORT = 3000
 const https = require("https")
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34fc0872209cf9fd64fd0566b4496eae"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34fc0872209cf9fd64fd0566b4496eae&units=metric"
 
 app.get("/", function(req,res){
     https.get(url, function(responce){
+        console.log(responce.statusCode)
         responce.on("data", function(data){
             const weatherData = JSON.parse(data)
-            console.log(weatherData)
-            console.log(weatherData.weather[0].description)
+            const iconName = weatherData.weather[0].icon
+            const URLName = "http://openweathermap.org/img/w/"+iconName+".png"
+            res.send(`
+                <h1>City : ${weatherData.name}</h1>
+                <h1>Description : ${weatherData.weather[0].description}</h1>
+                <h1>Tempreture : ${weatherData.main.temp}</h1>
+                <h1>Pressure : ${weatherData.main.pressure}</h1>
+                <img src="${URLName}"> 
+            `)
         })
     })
-    res.send("Server up and running")
 })
 
 app.listen(3000, function(){
