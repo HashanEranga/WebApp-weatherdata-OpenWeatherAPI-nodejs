448bbc9a804e3d2948323e1ee8f619f37da911c6
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-01-24 15:27:34 +0530

    feat(): get request is created

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const exp = require("express")
-
 const app = exp()
-
 const PORT = 3000
+const https = require("https")
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34fc0872209cf9fd64fd0566b4496eae"
 
 app.get("/", function(req,res){
+    https.get(url, function(responce){
+        console.log(responce)
+    })
     res.send("Server up and running")
 })
 
